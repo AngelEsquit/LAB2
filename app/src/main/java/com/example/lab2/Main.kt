ebfd45d62b85eefdb6abcf981691c922c068f977
AngelEsquit <esq23221@uvg.edu.gt>
2024-07-23 20:27:16 -0600

    Completo 2.0

--- app/src/main/java/com/example/lab2/Main.kt
+++ app/src/main/java/com/example/lab2/Main.kt
@@ -6,52 +6,41 @@ fun suma(numbers: List<Int>): Int {
 
 fun main () {
     // Ejercicio 1 - Promedio
-    var listaNumeros = listOf<Int>(1, 2, 3, 33)
-    val total = suma(listaNumeros)
-    val promedio = total.toDouble()/listaNumeros.size
-    println("Promedio: " + promedio) // Duda de decimales
+    val listaNumeros = listOf<Int>(1, 9, 5, 34)
+    println("Promedio: " + suma(listaNumeros).toDouble()/listaNumeros.size)
 
     // Ejercicio 2 - Filtrar impares
-    val numerosImpares = listaNumeros.filter { it % 2 != 0 }
-    println(numerosImpares)
+    println(listaNumeros.filter { it % 2 != 0 })
 
     // Ejercicio 3 - Palíndromo
-    val palabra = "aab"
-    if (palabra == palabra.reversed()) {
-        println("Es palíndromo.")
-    } else {
-        println("No es palíndromo")
-    }
+    val palabra = "aaa"
+    println(if (palabra == palabra.reversed()) "Es palíndromo." else "No es palíndromo.")
 
     // Ejercicio 4 - Saludos
     val nombres = listOf("Angel", "Esteban", "Javier", "Diego", "Elena")
-    val nombresSaludos = nombres.map {"¡Hola, $it!"}
-    println(nombresSaludos)
+    println(nombres.map {"¡Hola, $it!"})
 
     // Ejercicio 5 - PerformOperation
-    fun performOperation(numbers: List<Int>, operacion: String): Int {
-        // Definir las lambdas dentro de la función
-        val suma: (List<Int>) -> Int = { list -> list.sum() }
-        val resta: (List<Int>) -> Int = { list -> list.fold(numbers[0]) { acc, i -> acc - i } }
-
-        return when (operacion) {
-            "suma" -> suma(numbers)
-            "resta" -> resta(numbers)
-            else -> throw IllegalArgumentException("Operación no soportada: $operacion")
-        }
+    fun performOperation(a: Int, b: Int, operation: (Int, Int) -> Int): Int {
+        return operation(a, b)
     }
 
-    println(performOperation(listaNumeros, "resta"))
+    val sumaValores = performOperation(5, 3) { x, y -> x + y }
+    println(sumaValores)
+
+    val restaValores = performOperation(5, 3) { x, y -> x - y }
+    println(restaValores)
 
     // Ejercicio 6 - Persona y Estudiante
     data class Person(val name: String, val age: Int, val gender: String)
     data class Student(val name: String, val age: Int, val gender: String, val studentId: String)
 
     fun mapPerson (persona: Person): Student{
-        return Student(persona.name, persona.age, persona.gender, "")
+        return Student(persona.name, persona.age, persona.gender, "1")
     }
 
     val persona1 = Person("Diego", 18, "Hombre")
+
     val estudiante1 = mapPerson(persona1)
     println("El Estudiante ${estudiante1.name} tiene ${estudiante1.age}")
 }
